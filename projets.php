41c03af5169beaee821b348bf05ca6fb7451a1fd
GabinusMagnus <gabin_courdy@ikmail.com>
2024-11-28 09:37:48 +0100

    css onglets

--- projets.php
+++ projets.php
@@ -6,35 +6,32 @@
 </head>
 <body>
 <div class="topnav">
-    <a href="index.html/#accueil">Accueil</a>
-    <a href="index.html/#qualification">Qualification</a>
-    <a href="index.html/#veille">Veille</a>
+    <a href="index.html#accueil">Accueil</a>
+    <a href="index.html#qualification">Qualification</a>
+    <a href="index.html#veille">Veille</a>
     <a href="notes/Index_notes.php">Documentation</a>
     <a href="mentions_legales.html">Mentions légales</a>
 </div>
 <h1>Projets Professionnels :</h1>
 
+<div class="tabs">
 
-<ul class="tabs">
-  <li>
-    <input id="tab1" type="radio" name="tabs" checked />
-    <label for="tab1">Description</label>
-    <div id="tab-panel1" class="tab-panel">
-      <p>This is the content for the description panel.</p>
+    <div class="tab-item">
+        <input class="tab-input" type="radio" name="tabs" id="tab-1">
+        <label class="tab-label" for="tab-1">Tab 1</label>
+        <div class="tab-content">Content goes here</div>
     </div>
-  </li>
-  <li>
-    <input id="tab2" type="radio" name="tabs" />
-    <label for="tab2">Specifications</label>
-    <div id="tab-panel2" class="tab-panel">
-      <p>Specifications content can go here.</p>
+
+    <div class="tab-item">
+        <input class="tab-input" type="radio" name="tabs" id="tab-2">
+        <label class="tab-label" for="tab-2">Tab 2</label>
+        <div class="tab-content">Content goes here</div>
     </div>
-  </li>  
-  <li>
-    <input id="tab3" type="radio" name="tabs" />
-    <label for="tab3">Reviews</label>
-    <div id="tab-panel3" class="tab-panel">
-      <p>Here we can display some reviews from our customers.</p>
+
+    <div class="tab-item">
+        <input class="tab-input" type="radio" name="tabs" id="tab-3">
+        <label class="tab-label" for="tab-3">Tab 3</label>
+        <div class="tab-content">Content goes here</div>
     </div>
-  </li>     
-</ul>
+
+</div>
