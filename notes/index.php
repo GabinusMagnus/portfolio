64c88958089c37feaaacacb6a2d6683ca679d57e
GabinusMagnus <gabin_courdy@ikmail.com>
2024-11-28 17:43:10 +0100

    liste des notes fonctionnelle

--- notes/index.php
+++ notes/index.php
@@ -9,7 +9,7 @@
 Toutes les notes, toute la documentation accumulée au cours des deux ans de BTS.
 
 <div class="paper with-shadows">
-**Première année :**
+<h3>Première année :</h3>
 <details>
     <summary>
         Notes de première année :
@@ -20,12 +20,29 @@ Toutes les notes, toute la documentation accumulée au cours des deux ans de BTS
         foreach ($repertoire1 as $nom) {
             if ($nom != '.' and $nom != '..') {
             echo "<li><a href='" . "Première année/" . $nom . "'>" . $nom . "</a></li>";
-            }
+            };
         };
         ?>
         </ul>
     </summary>
-</detail>
+</details>
+<h3>Deuxième année :</h3>
+<details>
+    <summary>
+        Notes de deuxième année :
+        <ul>
+        <?php
+        $repertoire1 = scandir('./Deuxième année');
+        array_diff($repertoire1, array('..', '.'));
+        foreach ($repertoire1 as $nom) {
+            if ($nom != '.' and $nom != '..') {
+            echo "<li><a href='" . "Deuxième année/" . $nom . "'>" . $nom . "</a></li>";
+            };
+        };
+        ?>
+        </ul>
+    </summary>
+</details>
 </div>
 </body>
 </html>
