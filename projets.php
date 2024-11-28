7d6e2671c1afbed0c3bfa6e348d70a77a24ce963
GabinusMagnus <gabin_courdy@ikmail.com>
2024-11-28 09:28:31 +0100

    comm

--- projets.php
+++ projets.php
@@ -13,11 +13,23 @@
     <a href="mentions_legales.html">Mentions légales</a>
 </div>
 <h1>Projets Professionnels :</h1>
-<ul id="menu">
-    <li><a href="#tab1">First tab</a></li>
-    <li><a href="#tab2">Second tab</a></li>
-    <li><a href="#tab3">Third tab</a></li>
-</ul>
-<div id="tab1" class="tab-content">Content of first tab</div>
-<div id="tab2" class="tab-content">Content of second tab</div>
-<div id="tab3" class="tab-content">Content of third tab</div>
+<div class="tab-wrap">
+    <input type="radio" id="tab1" name="tabGroup1" class="tab" checked>
+    <label for="tab1">Short</label>
+    
+    <input type="radio" id="tab2" name="tabGroup1" class="tab">
+    <label for="tab2">Medium</label>
+    
+    <input type="radio" id="tab3" name="tabGroup1" class="tab">
+    <label for="tab3">Long</label>
+    
+    <div class="tab__content">
+        TAB ONE CONTENT
+    </div>
+    <div class="tab__content">
+      TAB Two CONTENT
+    </div>
+    <div class="tab__content">
+      TAB THREE CONTENT
+    </div>
+</div>
